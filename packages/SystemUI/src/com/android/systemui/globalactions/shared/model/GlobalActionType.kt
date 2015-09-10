52f11b414b90dca985adb3e16c55d0e190ccb3e8
Alexander Martinz <eviscerationls@gmail.com>
2015-09-09 18:07:01 -0700

    SystemUI: On-The-Go Mode (1/2)
    @neobuddy89: Updated for A14
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/shared/model/GlobalActionType.kt
+++ packages/SystemUI/src/com/android/systemui/globalactions/shared/model/GlobalActionType.kt
@@ -32,6 +32,7 @@ enum class GlobalActionType(val configKey: String) {
     LOGOUT("logout"),
     EMERGENCY("emergency"),
     SCREENSHOT("screenshot"),
+    ONTHEGO("onthego"),
     SYSTEM_UPDATE("system_update"),
     STANDBY("standby"),
     DEVICECONTROLS("device_controls");

587993fd5707ee2df680dd188a5fb30cce394148
someone5678 <someone5678@users.noreply.github.com>
2024-05-21 09:55:51 -0400

    base: Allow to hide screen capture status from apps [1/2]
    * Allow to hide screen capture detection from privacy invasive apps
    * This feature will be extended to screenrecord
      in the future release of Android (A15)
    
    Change-Id: Ie2a8e49614dcd8fce2311a644d615ab60670c094
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -11193,7 +11193,6 @@ public class WindowManagerService extends IWindowManager.Stub
         mSurfaceSyncGroupController.markSyncGroupReady(syncGroupToken);
     }
 
-
     /**
      * Must be called when a screenshot is taken via hardware chord.
      *
@@ -11207,7 +11206,9 @@ public class WindowManagerService extends IWindowManager.Stub
                 "notifyScreenshotListeners()")) {
             throw new SecurityException("Requires STATUS_BAR_SERVICE permission");
         }
+
         synchronized (mGlobalLock) {
+            if (shouldHideScreenCapture()) return new ArrayList<>();
             final DisplayContent displayContent = mRoot.getDisplayContent(displayId);
             if (displayContent == null) {
                 return new ArrayList<>();
@@ -11490,6 +11491,10 @@ public class WindowManagerService extends IWindowManager.Stub
     @EnforcePermission(android.Manifest.permission.DETECT_SCREEN_RECORDING)
     @Override
     public boolean registerScreenRecordingCallback(IScreenRecordingCallback callback) {
+        if (shouldHideScreenCapture()) {
+            return false;
+        }
+
         registerScreenRecordingCallback_enforcePermission();
         return mScreenRecordingCallbackController.register(callback);
     }
@@ -11502,6 +11507,10 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     void onProcessActivityVisibilityChanged(int uid, boolean visible) {
+        if (shouldHideScreenCapture()) {
+            return;
+        }
+
         mScreenRecordingCallbackController.onProcessActivityVisibilityChanged(uid, visible);
     }
 
@@ -11630,5 +11639,10 @@ public class WindowManagerService extends IWindowManager.Stub
                 mAtmService.updateFontScaleIfNeeded(userId);
             }
         }
+    }
+
+    private boolean shouldHideScreenCapture() {
+        return Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.HIDE_SCREEN_CAPTURE_STATUS, 0) != 0;
     }
 }

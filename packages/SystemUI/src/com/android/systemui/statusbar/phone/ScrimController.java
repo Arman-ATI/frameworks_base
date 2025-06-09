1d56990d3ebe1c2f19f16ae5e896ff5f4146f008
ruyue <ruyueee@outlook.com>
2025-06-09 13:54:28 +0800

    SystemUI: Remove the scrimView on lock screen
    Change-Id: Ia710f6a6d61290d25642e1de73fe7deddbc74cf5
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -1116,6 +1116,12 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             Pair<Integer, Float> result = calculateBackStateForState(mState);
             int behindTint = result.first;
             float behindAlpha = result.second;
+
+            if (mState == ScrimState.KEYGUARD) {
+                behindAlpha = 0.0f;
+                behindTint = Color.TRANSPARENT; 
+            }
+            
             if (mTransitionToFullShadeProgress > 0.0f) {
                 Pair<Integer, Float> shadeResult = calculateBackStateForState(
                         ScrimState.SHADE_LOCKED);

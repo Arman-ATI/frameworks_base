55306e712714bfb1ef44b4159cc63a77a81ec658
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-05 06:43:05 +0800

    SystemUI: Add lockscreen media art
    Squashed:
    
        From: rmp22 <195054967+rmp22@users.noreply.github.com>
        Date: Thu, 30 Oct 2025 19:58:37 +0800
        Subject: [PATCH] [media_art] removing retry mechanism
    
        Change-Id: Id59bd09fd112caf7f6e3d3aef06066f94ee41faf
    
    Change-Id: I4db66c3e1e98c1ac04a315e8e76ec3799e8f4cbd
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7311,6 +7311,12 @@ public final class Settings {
         public static final String QS_BRIGHTNESS_SLIDER_HAPTIC = "qs_brightness_slider_haptic";
 
         /**
+         * Lockscreen Media Art
+         * @hide
+         */
+        public static final String LS_MEDIA_ART_ENABLED = "ls_media_art_enabled";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -74,6 +74,7 @@ import android.view.MotionEvent;
 import android.view.SurfaceControl;
 import android.view.ThreadedRenderer;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.WindowManagerGlobal;
 import android.view.accessibility.AccessibilityManager;
@@ -131,6 +132,7 @@ import com.android.systemui.keyguard.KeyguardViewMediator;
 import com.android.systemui.keyguard.ScreenLifecycle;
 import com.android.systemui.keyguard.WakefulnessLifecycle;
 import com.android.systemui.log.SessionTracker;
+import com.android.systemui.media.MediaViewController;
 import com.android.systemui.media.NotificationMediaManager;
 import com.android.systemui.navigationbar.NavigationBarController;
 import com.android.systemui.navigationbar.views.NavigationBarView;
@@ -405,6 +407,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     private final WallpaperManager mWallpaperManager;
     private final UserTracker mUserTracker;
     private final ActivityStarter mActivityStarter;
+    private final MediaViewController mMediaViewController;
 
     private final DisplayMetrics mDisplayMetrics;
 
@@ -614,7 +617,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
             QuickAccessWalletController walletController,
             WindowManager windowManager,
             WindowManagerProvider windowManagerProvider,
-            SessionTracker sessionTracker
+            SessionTracker sessionTracker,
+            MediaViewController mediaViewController
     ) {
         mContext = context;
         mNotificationsController = notificationsController;
@@ -736,6 +740,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         mWindowManager = windowManager;
         mWindowManagerProvider = windowManagerProvider;
         mSessionTracker = sessionTracker;
+        mMediaViewController = mediaViewController;
     }
 
     private void initBubbles(Bubbles bubbles) {
@@ -876,6 +881,13 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                 (requestTopUi, componentTag) -> mMainExecutor.execute(
                         () -> mTopUiController.setRequestTopUi(requestTopUi, componentTag)
                 )));
+        getNotifContainerParentView().addView(mMediaViewController.getMediaArtScrim(), 0);
+    }
+
+    private ViewGroup getNotifContainerParentView() {
+        ViewGroup rootView = (ViewGroup) getNotificationShadeWindowView().findViewById(R.id.scrim_behind).getParent();
+        ViewGroup targetView = rootView.findViewById(R.id.notification_container_parent);
+        return targetView;
     }
 
     @VisibleForTesting

1ee7b6e563b976bffd30bef833373a1b9a7de9cc
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-05 10:17:41 +0800

    SystemUI: Fixing InternetTileNewImpl secondary click
    Change-Id: Ib9420f5fb7dd0f249c6703a60b151b78fd6a5c24
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/InternetTileNewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/InternetTileNewImpl.kt
@@ -21,6 +21,7 @@ import android.os.Handler
 import android.os.Looper
 import android.provider.Settings
 import android.widget.Button
+import com.android.settingslib.net.DataUsageController
 import com.android.internal.logging.MetricsLogger
 import com.android.systemui.animation.Expandable
 import com.android.systemui.dagger.qualifiers.Background
@@ -37,8 +38,11 @@ import com.android.systemui.qs.logging.QSLogger
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.dialog.InternetDetailsViewModel
 import com.android.systemui.qs.tiles.dialog.InternetDialogManager
+import com.android.systemui.qs.tiles.dialog.WifiStateWorker
 import com.android.systemui.res.R
+import com.android.systemui.Prefs
 import com.android.systemui.statusbar.connectivity.AccessPointController
+import com.android.systemui.statusbar.connectivity.NetworkController
 import com.android.systemui.statusbar.pipeline.shared.ui.binder.InternetTileBinder
 import com.android.systemui.statusbar.pipeline.shared.ui.model.InternetTileModel
 import com.android.systemui.statusbar.pipeline.shared.ui.viewmodel.InternetTileViewModel
@@ -60,6 +64,8 @@ constructor(
     private val internetDialogManager: InternetDialogManager,
     private val accessPointController: AccessPointController,
     private val internetDetailsViewModelFactory: InternetDetailsViewModel.Factory,
+    private val wifiStateWorker: WifiStateWorker,
+    private val networkController: NetworkController
 ) :
     QSTileImpl<QSTile.BooleanState>(
         host,
@@ -73,6 +79,25 @@ constructor(
         qsLogger,
     ) {
     private var model: InternetTileModel = viewModel.tileModel.value
+    private val dataUsageController: DataUsageController = networkController.mobileDataController
+    
+    private var dataEnabled: Boolean
+        get() = dataUsageController.isMobileDataEnabled()
+        set(value) {
+            dataUsageController.setMobileDataEnabled(value)
+        }
+
+    private var wifiEnabled: Boolean
+        get() = wifiStateWorker.isWifiEnabled()
+        set(value) {
+            wifiStateWorker.setWifiEnabled(value)
+        }
+
+    private var lastUsedModel: Int
+        get() = Prefs.getInt(mContext, "internet_tile_model", WIFI)
+        set(value) {
+            Prefs.putInt(mContext, "internet_tile_model", value)
+        }
 
     init {
         InternetTileBinder.bind(lifecycle, viewModel.tileModel) { newModel ->
@@ -89,7 +114,10 @@ constructor(
         mContext.getString(R.string.quick_settings_internet_label)
 
     override fun newTileState(): QSTile.BooleanState {
-        return QSTile.BooleanState().also { it.forceExpandIcon = true }
+        return QSTile.BooleanState().also {
+             it.forceExpandIcon = true
+             it.handlesSecondaryClick = true
+        }
     }
 
     override fun handleClick(expandable: Expandable?) {
@@ -100,6 +128,26 @@ constructor(
                 accessPointController.canConfigWifi(),
                 expandable,
             )
+        }
+    }
+
+    override fun handleSecondaryClick(expandable: Expandable?) {
+        when {
+            wifiEnabled -> {
+                wifiEnabled = false
+                lastUsedModel = WIFI
+            }
+            dataEnabled -> {
+                dataEnabled = false
+                lastUsedModel = DATA
+            }
+            else -> {
+                if (lastUsedModel == WIFI) {
+                    wifiEnabled = true
+                } else {
+                    dataEnabled = true
+                }
+            }
         }
     }
 
@@ -120,5 +168,7 @@ constructor(
         const val TILE_SPEC: String = "internet"
 
         private val WIFI_SETTINGS = Intent(Settings.ACTION_WIFI_SETTINGS)
+        const val WIFI = 0
+        const val DATA = 1
     }
 }

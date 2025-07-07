26462c97b37f49d5bd9bf7469f3fbce8176b406b
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-07 11:59:23 +0800

    SystemUI: Add option to disable quicksettings on keyguard
    reverse-engineered from nothingOS - simplified
    
    Change-Id: Id2910cdc4e753609be3cbe7817605e27b5f6a53f
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14122,6 +14122,11 @@ public final class Settings {
         public static final String ENABLE_PROJECTION_PRIVACY_INDICATOR = "enable_projection_privacy_indicator";
 
         /**
+         * @hide
+         */
+        public static final String ENABLE_LOCKSCREEN_QUICK_SETTINGS = "enable_lockscreen_quick_settings";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *

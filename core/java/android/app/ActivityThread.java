295ec7a70d467aaf3b9cfd398dc070bb1225e3c2
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-10-03 19:09:21 +0800

    core: Adding dynamic font feature
    optimized overriding of system fonts without reflection usage and wider font override support
    
    Change-Id: I4940b525584a3ad7c83193ff2484e6726c5437f9
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -8095,6 +8095,9 @@ public final class ActivityThread extends ClientTransactionHandler
         data.info = getPackageInfo(data.appInfo, mCompatibilityInfo, null /* baseLoader */,
                 false /* securityViolation */, true /* includeCode */,
                 false /* registerPackage */, isSdkSandbox);
+
+        Typeface.changeFont();
+
         if (isSdkSandbox) {
             data.info.setSdkSandboxStorage(data.sdkSandboxClientAppVolumeUuid,
                     data.sdkSandboxClientAppPackage);

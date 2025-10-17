1bbd722b08740c0a144786595778853aa79294df
Pranav Vashi <neobuddy89@gmail.com>
2025-10-18 01:35:16 +0530

    base: Allow screen off UDFPS when configured
    As per config.xml
    - config_screen_off_udfps_enabled: Whether to enable fp unlock when screen turns off on udfps devices
    - config_screen_off_udfps_default_on: Default value for fp screen off unlock toggle
    
    Use both these configs properly in AmbientDisplayConfiguration as well as UdfpsController.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -174,11 +174,10 @@ public class AmbientDisplayConfiguration {
         boolean isScreenOffUnlockEnabled = mScreenOffUdfpsAvailable && Flags.screenOffUnlockUdfps();
         if (!isScreenOffUnlockEnabled) {
             return false;
-        } else {
-            return boolSetting(SCREEN_OFF_UNLOCK_UDFPS_ENABLED, user,
-                    mContext.getResources().getBoolean(R.bool.config_screen_off_udfps_default_on)
-                            ? 1 : 0);
         }
+        return mContext.getResources().getBoolean(R.bool.config_screen_off_udfps_default_on)
+                ? boolSettingDefaultOn(SCREEN_OFF_UNLOCK_UDFPS_ENABLED, user)
+                : boolSettingDefaultOff(SCREEN_OFF_UNLOCK_UDFPS_ENABLED, user);
     }
 
     /** @hide */

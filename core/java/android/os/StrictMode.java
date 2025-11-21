4435463c527cb7758d26bdee03597460d55f0a51
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-21 08:55:32 +0800

    core: Preventing memory leaks from bloating os memory that leads to OOM
    thank you meta :) million dollar company btw
    
    11-16 12:32:07.427 12602 12602 E ActivityThread: Activity com.facebook.messenger.neue.MainActivity has leaked IntentReceiver X.0Cl@f8f79db that was originally registered here. Are you missing a call to unregisterReceiver()?
    11-16 12:32:07.427 12602 12602 E ActivityThread: android.app.IntentReceiverLeaked: Activity com.facebook.messenger.neue.MainActivity has leaked IntentReceiver X.0Cl@f8f79db that was originally registered here. Are you missing a call to unregisterReceiver()?
    11-16 12:32:07.427 12602 12602 E ActivityThread: 	at android.app.LoadedApk$ReceiverDispatcher.<init>(LoadedApk.java:1879)
    11-16 12:32:07.427 12602 12602 E ActivityThread: 	at android.app.LoadedApk.getReceiverDispatcher(LoadedApk.java:1621)
    11-16 12:32:07.427 12602 12602 E ActivityThread: 	at android.app.ContextImpl.registerReceiverInternal(ContextImpl.java:1912)
    11-16 12:32:07.427 12602 12602 E ActivityThread: 	at android.app.ContextImpl.registerReceiver(ContextImpl.java:1871)
    11-16 12:32:07.427 12602 12602 E ActivityThread: 	at android.app.ContextImpl.registerReceiver(ContextImpl.java:1858)
    11-16 12:32:07.427 12602 12602 E ActivityThread: 	at android.content.ContextWrapper.registerReceiver(ContextWrapper.java:784)
    
    Change-Id: I4b529c2876c823dc21e4780e6beaa997f7be0425
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/os/StrictMode.java
+++ core/java/android/os/StrictMode.java
@@ -1633,6 +1633,8 @@ public final class StrictMode {
         if (targetSdkVersion >= Build.VERSION_CODES.N) {
             builder.detectFileUriExposure();
             builder.penaltyDeathOnFileUriExposure();
+            builder.detectActivityLeaks();
+            builder.detectLeakedRegistrationObjects();
         }
 
         if (Build.IS_USER || Build.IS_USERDEBUG || DISABLE || SystemProperties.getBoolean(DISABLE_PROPERTY, false)) {
@@ -2758,7 +2760,9 @@ public final class StrictMode {
 
     /** @hide */
     public static void onVmPolicyViolation(Violation originStack) {
-        onVmPolicyViolation(originStack, false);
+        boolean forceDeath = originStack instanceof IntentReceiverLeakedViolation 
+            || originStack instanceof ServiceConnectionLeakedViolation;
+        onVmPolicyViolation(originStack, forceDeath);
     }
 
     /** @hide */

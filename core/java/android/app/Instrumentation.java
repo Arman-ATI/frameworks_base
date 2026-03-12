cf406d85487a53a9ec35acc6c14253f35fc5f9ae
kenway214 <kenway214@outlook.com>
2026-03-12 13:10:35 +0000

    core: Introduce per-apps spoofing [1/2]
    - Device models taken from PixelPropsUtils
    
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -82,6 +82,7 @@ import java.util.concurrent.TimeoutException;
 
 import com.android.internal.util.android.AttestationHooks;
 import com.android.internal.util.android.PixelPropsUtils;
+import com.android.internal.util.android.PerAppsPropsUtils;
 
 /**
  * Base class for implementing application instrumentation code.  When running
@@ -1366,6 +1367,7 @@ public class Instrumentation {
         app.attach(context);
         AttestationHooks.setProps(context);
         PixelPropsUtils.setProps(context);
+        PerAppsPropsUtils.setProps(context);
         return app;
     }
     
@@ -1386,6 +1388,7 @@ public class Instrumentation {
         app.attach(context);
         AttestationHooks.setProps(context);
         PixelPropsUtils.setProps(context);
+        PerAppsPropsUtils.setProps(context);
         return app;
     }
 

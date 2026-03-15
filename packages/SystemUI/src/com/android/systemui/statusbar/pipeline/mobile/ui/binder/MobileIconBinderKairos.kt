935177c2f35aa4a413c24b60a21b1bc2883cfab7
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-03-15 09:46:20 +0800

    base: Adding theme engine interface
    Change-Id: I64d455835389acb2e591ff0b623797e1e122f06d
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/binder/MobileIconBinderKairos.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/binder/MobileIconBinderKairos.kt
@@ -37,6 +37,7 @@ import com.android.systemui.kairos.util.nameTag
 import com.android.systemui.plugins.DarkIconDispatcher
 import com.android.systemui.res.R
 import com.android.systemui.statusbar.StatusBarIconView
+import com.android.systemui.statusbar.connectivity.ThemeIconController
 import com.android.systemui.statusbar.pipeline.mobile.domain.model.SignalIconModel
 import com.android.systemui.statusbar.pipeline.mobile.ui.MobileViewLogger
 import com.android.systemui.statusbar.pipeline.mobile.ui.viewmodel.LocationBasedMobileViewModelKairos
@@ -147,6 +148,22 @@ object MobileIconBinderKairos {
         val dotView = view.requireViewById<StatusBarIconView>(R.id.status_bar_dot)
 
         val isVisible = viewModel.isVisible.sample()
+
+        var lastCellularIconKairos: SignalIconModel.Cellular? = null
+        val refreshCallbackKairos = Runnable {
+            val icon = lastCellularIconKairos ?: return@Runnable
+            val themed = ThemeIconController
+                .getThemedSignalIcon(view.context, icon.level, icon.numberOfLevels)
+            if (themed != null) {
+                iconView.setImageDrawable(themed)
+            } else {
+                iconView.setImageDrawable(mobileDrawable)
+                mobileDrawable.level = icon.toSignalDrawableState()
+            }
+            mobileGroupView.invalidate()
+        }
+        ThemeIconController.registerRefreshCallback(refreshCallbackKairos)
+
         effect(name = nameTag("MobileIconBinderKairos.viewIsVisibleInitEffect")) {
             view.isVisible = isVisible
             iconView.isVisible = true
@@ -185,6 +202,8 @@ object MobileIconBinderKairos {
                 } finally {
                     binding.isCollecting = false
                     logger.logCollectionStopped(view, viewModel)
+                    ThemeIconController.unregisterRefreshCallback(
+                        refreshCallbackKairos)
                 }
             }
 
@@ -216,6 +235,7 @@ object MobileIconBinderKairos {
                         else -> false
                     }
                 if (newIcon is SignalIconModel.CellularTypeIconModel) {
+                    lastCellularIconKairos = newIcon
                     val packedSignalDrawableState = newIcon.toSignalDrawableState()
                     viewModel.verboseLogger?.logBinderReceivedSignalCellularTypeIcon(
                         parentView = view,
@@ -224,8 +244,18 @@ object MobileIconBinderKairos {
                         packedSignalDrawableState = packedSignalDrawableState,
                         shouldRequestLayout = shouldRequestLayout,
                     )
-                    iconView.setImageDrawable(mobileDrawable)
-                    mobileDrawable.level = packedSignalDrawableState
+                    val themedDrawable = ThemeIconController
+                        .getThemedSignalIcon(
+                            view.context,
+                            newIcon.level,
+                            newIcon.numberOfLevels
+                        )
+                    if (themedDrawable != null) {
+                        iconView.setImageDrawable(themedDrawable)
+                    } else {
+                        iconView.setImageDrawable(mobileDrawable)
+                        mobileDrawable.level = packedSignalDrawableState
+                    }
                     viewModel.verboseLogger?.logBinderSignalIconResult(
                         parentView = view,
                         subId = viewModel.subscriptionId,

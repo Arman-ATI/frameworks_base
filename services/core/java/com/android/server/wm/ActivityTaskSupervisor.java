ad09af8f552354747207c71ed3e4b3b30fd0cc86
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-03-22 00:08:30 +0800

    Add support for game space
    Change-Id: Iaaf20bcbaa50c42dd6b7739ae233b245a2c5ad5c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
+++ services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
@@ -1912,6 +1912,7 @@ public class ActivityTaskSupervisor implements RecentTasks.Callbacks {
             mBalController.checkActivityAllowedToClearTask(
                             task, callingUid, callingPid, callerActivityClassName);
             AxSandboxService.get().removeTask(task, reason);
+            GameSpaceService.get().removeTask(task, reason);
         } finally {
             task.mInRemoveTask = false;
             mService.mChainTracker.endPartial();

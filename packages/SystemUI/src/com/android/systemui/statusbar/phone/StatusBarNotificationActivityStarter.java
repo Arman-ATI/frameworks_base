edb769520b28bd3cee1d428b117b4901ec0eac8a
Dmitrii <bankersenator@gmail.com>
2026-03-24 10:03:21 +0000

    SystemUI: Toggle to launch notification apps in bubble [1/2]
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarNotificationActivityStarter.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarNotificationActivityStarter.java
@@ -38,6 +38,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.RemoteException;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.service.dreams.IDreamManager;
 import android.service.notification.StatusBarNotification;
 import android.text.TextUtils;
@@ -467,10 +468,17 @@ public class StatusBarNotificationActivityStarter implements NotificationActivit
                     remoteInputText.toString());
         }
         final boolean canBubble = entry.canBubble();
-        if (canBubble) {
+        boolean forceBubbleApps = Settings.System.getIntForUser(mContext.getContentResolver(),
+                "notification_bubble_apps", 0, UserHandle.USER_CURRENT) == 1;
+
+        if (canBubble || forceBubbleApps) {
             mLogger.logExpandingBubble(entry);
             removeHunAfterClick(row);
-            expandBubbleStackOnMainThread(entry);
+            if (forceBubbleApps && !entry.isBubble()) {
+                expandAppBubbleStackOnMainThread(intent, entry.getSbn().getUser());
+            } else {
+                expandBubbleStackOnMainThread(entry);
+            }
         } else {
             startNotificationIntent(intent, fillInIntent, entry, row, animate, isActivityIntent);
         }
@@ -584,6 +592,23 @@ public class StatusBarNotificationActivityStarter implements NotificationActivit
             mLogger.logSendPendingIntent(entry, intent, result);
             return result;
         }
+    }
+
+    private void expandAppBubbleStackOnMainThread(PendingIntent intent, UserHandle user) {
+        if (!mBubblesManagerOptional.isPresent()) {
+            return;
+        }
+
+        if (Looper.getMainLooper().isCurrentThread()) {
+            expandAppBubbleStack(intent, user);
+        } else {
+            mMainThreadHandler.post(() -> expandAppBubbleStack(intent, user));
+        }
+    }
+
+    private void expandAppBubbleStack(PendingIntent intent, UserHandle user) {
+        mBubblesManagerOptional.get().expandStackAndSelectBubble(intent, user);
+        mShadeController.collapseShade();
     }
 
     private void startNotificationIntent(

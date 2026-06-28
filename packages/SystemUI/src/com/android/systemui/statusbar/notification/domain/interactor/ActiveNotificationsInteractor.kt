9e3e17000527d87a6ccb4c5688e141730faf09d6
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-06-29 06:58:22 +0800

    SystemUI: Show footer for essential notifications
    Change-Id: Ib6aefb146627eebdcfd64c5ef18c57fedd625979
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/domain/interactor/ActiveNotificationsInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/domain/interactor/ActiveNotificationsInteractor.kt
@@ -109,6 +109,16 @@ constructor(
             .distinctUntilChanged()
             .flowOn(backgroundDispatcher)
 
+    val hasEssentialNotifications: Flow<Boolean> =
+        repository.activeNotifications
+            .map { store ->
+                store.bundles.values.any { bundle ->
+                    bundle.isEssential && bundle.children.isNotEmpty()
+                }
+            }
+            .distinctUntilChanged()
+            .flowOn(backgroundDispatcher)
+
     /**
      * The same as [areAnyNotificationsPresent], but without flows, for easy access in synchronous
      * code.

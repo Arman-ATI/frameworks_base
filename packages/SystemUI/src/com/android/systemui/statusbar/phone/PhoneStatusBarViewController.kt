d6f0e55a2171954db8570cf1be262913c2318e42
keosh <conquer2152016@gmail.com>
2026-07-12 21:29:45 +0000

    [SystemUI] SystemIconsPopup: Show power menu via GlobalActionsComponent
    GlobalActionsDialogLite is unscoped, so injecting it created a second
    instance whose broadcast receiver, content observers and configuration
    callbacks were registered in its constructor and never destroyed. Route
    the popup's power action through the singleton GlobalActionsComponent,
    which also applies the real keyguard, provisioning and display state.
    
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarViewController.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarViewController.kt
@@ -29,7 +29,7 @@ import android.view.WindowManager
 import android.os.VibrationEffect
 import androidx.annotation.VisibleForTesting
 import com.android.systemui.Gefingerpoken
-import com.android.systemui.globalactions.GlobalActionsDialogLite
+import com.android.systemui.globalactions.GlobalActionsComponent
 import com.android.systemui.display.dagger.SystemUIDisplaySubcomponent.DisplayAware
 import com.android.systemui.plugins.DarkIconDispatcher
 import com.android.systemui.res.R
@@ -92,7 +92,7 @@ private constructor(
     private val lazyShadeDisplaysRepository: Lazy<ShadeDisplaysRepository>,
     private val statusBarWindowControllerStore: StatusBarWindowControllerStore,
     private val tunerService: TunerService,
-    private val globalActionsDialog: GlobalActionsDialogLite,
+    private val globalActionsComponent: Lazy<GlobalActionsComponent>,
     private val windowManager: WindowManager,
 ) : ViewController<PhoneStatusBarView>(view), TunerService.Tunable {
 
@@ -246,9 +246,9 @@ private constructor(
         systemIconsPopupController = SystemIconsPopupController(
             context = context,
             windowManager = windowManager,
-            globalActionsDialog = globalActionsDialog
+            onShowPowerMenu = { globalActionsComponent.get().handleShowGlobalActionsMenu() }
         )
-        
+
         // Set up long click listener for system icons popup
         endSideContainer.setOnLongClickListener { toggleSystemIconsPopup() }
         endSideContainer.isLongClickable = true
@@ -567,7 +567,7 @@ private constructor(
         private val lazyShadeDisplaysRepository: Lazy<ShadeDisplaysRepository>,
         private val statusBarWindowControllerStore: StatusBarWindowControllerStore,
         private val tunerService: TunerService,
-        private val globalActionsDialog: GlobalActionsDialogLite,
+        private val globalActionsComponent: Lazy<GlobalActionsComponent>,
         private val windowManager: WindowManager,
     ) {
         fun create(view: PhoneStatusBarView): PhoneStatusBarViewController {
@@ -595,7 +595,7 @@ private constructor(
                 lazyShadeDisplaysRepository,
                 statusBarWindowControllerStore,
                 tunerService,
-                globalActionsDialog,
+                globalActionsComponent,
                 windowManager,
             )
         }

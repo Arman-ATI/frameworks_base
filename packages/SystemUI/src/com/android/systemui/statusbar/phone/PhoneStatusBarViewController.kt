d69ccfc92f53f38b2bfda502bf4cced42a895e6f
Ghosuto <clash.raja10@gmail.com>
2026-07-15 03:58:02 +0000

    SystemUI: Make StatusBar icon popup configurable [1/2]
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarViewController.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarViewController.kt
@@ -106,6 +106,7 @@ private constructor(
     private var extraStartDp = 0
     private var extraTopDp = 0
     private var extraEndDp = 0
+    private var isSystemIconsPopupEnabled = false
 
     private var systemIconsPopupController: SystemIconsPopupController? = null
     private val vibrator = context.getSystemService(android.os.Vibrator::class.java)
@@ -220,6 +221,7 @@ private constructor(
         tunerService.addTunable(this, STATUSBAR_EXTRA_PADDING_START)
         tunerService.addTunable(this, STATUSBAR_EXTRA_PADDING_TOP)
         tunerService.addTunable(this, STATUSBAR_EXTRA_PADDING_END)
+        tunerService.addTunable(this, STATUSBAR_SYSTEM_ICONS_POPUP_ENABLED)
     }
 
     override fun onTuningChanged(key: String?, newValue: String?) {
@@ -230,6 +232,10 @@ private constructor(
                 extraTopDp = TunerService.parseInteger(newValue, 0)
             STATUSBAR_EXTRA_PADDING_END ->
                 extraEndDp = TunerService.parseInteger(newValue, 0)
+            STATUSBAR_SYSTEM_ICONS_POPUP_ENABLED -> {
+                isSystemIconsPopupEnabled = TunerService.parseIntegerSwitch(newValue, false)
+                updateSystemIconsPopupLongClickListener()
+            }
             else -> return
         }
 
@@ -245,7 +251,7 @@ private constructor(
             onShowPowerMenu = { globalActionsComponent.get().handleShowGlobalActionsMenu() }
         )
 
-        endSideContainer.setOnLongClickListener { toggleSystemIconsPopup() }
+        updateSystemIconsPopupLongClickListener()
 
         endSideContainer.setOnHoverListener(
             statusOverlayHoverListenerFactory.createDarkAwareListener(endSideContainer)
@@ -281,6 +287,16 @@ private constructor(
                 customHeightPx = iconContainerHeightPx,
             )
         )
+    }
+
+    private fun updateSystemIconsPopupLongClickListener() {
+        if (!::endSideContainer.isInitialized) return
+        if (isSystemIconsPopupEnabled) {
+            endSideContainer.setOnLongClickListener { toggleSystemIconsPopup() }
+        } else {
+            endSideContainer.setOnLongClickListener(null)
+            systemIconsPopupController?.let { if (it.isShowing) it.hidePopup() }
+        }
     }
 
     private fun statusBarTapToExpandShadeEnabled(): Boolean {
@@ -524,6 +540,8 @@ private constructor(
             "system:" + Settings.System.STATUSBAR_EXTRA_PADDING_TOP
         private const val STATUSBAR_EXTRA_PADDING_END =
             "system:" + Settings.System.STATUSBAR_EXTRA_PADDING_END
+        private const val STATUSBAR_SYSTEM_ICONS_POPUP_ENABLED =
+            "system:" + Settings.System.STATUSBAR_SYSTEM_ICONS_POPUP_ENABLED
     }
 
     class Factory

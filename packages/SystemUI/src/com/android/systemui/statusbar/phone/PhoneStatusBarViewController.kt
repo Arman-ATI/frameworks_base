c5ed47f76ddf7bea145ad9f62cf6bd442187856c
Ghosuto <clash.raja10@gmail.com>
2026-07-16 11:30:16 +0000

    SystemUI: SystemIconsPopup: Persist toggle states across popup without re-querying
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarViewController.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarViewController.kt
@@ -351,7 +351,7 @@ private constructor(
 
     @VisibleForTesting
     public override fun onViewDetached() {
-        systemIconsPopupController?.hidePopup()
+        systemIconsPopupController?.destroy()
         systemIconsPopupController = null
         tunerService.removeTunable(this)
         removeDarkReceivers()

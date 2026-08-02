27c844a036c5217ff0fe4624ab6aa159ef2d1b33
Arman-ATI <altafiarman5@gmail.com>
2026-08-02 20:12:18 +0330

    fix24

--- services/voiceinteraction/java/com/android/server/voiceinteraction/VoiceInteractionManagerService.java
+++ services/voiceinteraction/java/com/android/server/voiceinteraction/VoiceInteractionManagerService.java
@@ -125,7 +125,7 @@ import com.android.internal.compat.IPlatformCompat;
 import com.android.internal.content.PackageMonitor;
 import com.android.internal.os.BackgroundThread;
 import com.android.internal.util.DumpUtils;
-import com.android.internal.util.lunaris.Utils;
+import com.android.internal.util.android.Utils;
 import com.android.server.FgThread;
 import com.android.server.LocalServices;
 import com.android.server.SoundTriggerInternal;

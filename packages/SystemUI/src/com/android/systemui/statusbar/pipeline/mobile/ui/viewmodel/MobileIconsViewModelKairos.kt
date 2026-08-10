VoltageOS-staging/frameworks_base_new

SystemUI: fix roaming icon
after google changes in QPR2
Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/viewmodel/MobileIconsViewModelKairos.kt
@@ -193,6 +193,7 @@ constructor(
             override val contentDescription: State<MobileContentDescription?> =
                 latest(null) { contentDescription }
             override val roaming: State<Boolean> = latest(false) { roaming }
+            override val isRoamingVisible: KairosState<Boolean> = stateOf(false)
             override val networkTypeIcon: State<Icon.Resource?> = latest(null) { networkTypeIcon }
             override val networkTypeBackground: State<Icon.Resource?> =
                 latest(null) { networkTypeBackground }

Arman-ATI/frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/viewmodel/MobileIconsViewModelKairos.kt
@@ -200,6 +200,7 @@ constructor(
             override val contentDescription: State<MobileContentDescription?> =
                 latest(null) { contentDescription }
             override val roaming: State<Boolean> = latest(false) { roaming }
+            override val isRoamingVisible: State<Boolean> = stateOf(false)
             override val networkTypeIcon: State<Icon.Resource?> = latest(null) { networkTypeIcon }
             override val networkTypeBackground: State<Icon.Resource?> =
                 latest(null) { networkTypeBackground }